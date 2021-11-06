6dc5c2350e09809f05573253e6816e375a4866cd
parkeunsik <qkrdmstlr3@naver.com>
2021-11-06 20:58:43 +0900

    [feat] Marker component 생성
    - 핀 역할을 하는 마커 컴포넌트 생성
    - 추가 / 제거로 나누어서 함수 구현

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Map from '../components/Map';
+import Marker from '../components/Marker';
 
-function index() {
+// TODO: remove
+const dummy = [{ lng: -86.89871737888747, lat: 40.41866254968954 }];
+
+function Main() {
+  const [map, setMap] = useState();
+
   return (
-    <>
-      <Map />
-    </>
+    <Map getMap={setMap}>
+      <Marker map={map} markers={dummy} />
+    </Map>
   );
 }
 
-export default index;
+export default Main;
