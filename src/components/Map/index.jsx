8447d340659cdf407d6ccc04819de8540ca3c7c2
parkeunsik <qkrdmstlr3@naver.com>
2021-11-06 13:40:39 +0900

    [feat] geocoder 추가
    - 검색을 위해서 mapbox geocoder추가
    - 몇 가지 테스트를 해보니 완전하지않은 검색이 많아 다른 geocoder사용방안을 고려해보아야 할 듯

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,27 +1,16 @@
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 mapboxgl.accessToken = process.env.MAPBOX_KEY;
 
 const LNG = -86.89871737888747;
 const LAT = 40.41866254968954;
 const ZOOM = 12;
-const LABELS = [
-  'country-label',
-  'settlement-label',
-  'road-label',
-  'state-label',
-  'settlement-subdivision-label',
-  'airport-label',
-  'transit-label',
-  'water-point-label',
-  'water-line-label',
-  'waterway-label',
-  'natural-point-label',
-  'natural-line-label',
-];
 
 const MapContainer = styled.div`
   position: absolute;
@@ -29,6 +18,26 @@ const MapContainer = styled.div`
   left: 0 !important;
   right: 0 !important;
   top: 0 !important;
+
+  .mapboxgl-ctrl-logo,
+  .mapboxgl-ctrl-attrib {
+    display: none !important;
+  }
+`;
+
+const SearchBox = styled.div`
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  border-radius: 20px;
+
+  input[type='text'] {
+    height: 40px;
+    outline: none;
+  }
+  svg {
+    top: 9px;
+  }
 `;
 
 function Map({ children }) {
@@ -38,19 +47,27 @@ function Map({ children }) {
     const map = new mapboxgl.Map({
       container: mapRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      locale: ['get', 'name_ko'],
       center: [LNG, LAT],
       zoom: ZOOM,
     });
 
-    map.on('load', () => {
-      LABELS.forEach((label) => {
-        map.setLayoutProperty(label, 'text-field', ['get', 'name_ko']);
-      });
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      placeholder: '검색 후 핀을 누르세요',
+      mapboxgl,
     });
-  });
 
-  return <MapContainer ref={mapRef}>{children}</MapContainer>;
+    map.on('load', () => {
+      document.getElementById('search-bar').appendChild(geocoder.onAdd(map));
+    });
+  }, []);
+
+  return (
+    <MapContainer ref={mapRef}>
+      <SearchBox id="search-bar" />
+      {children}
+    </MapContainer>
+  );
 }
 
 Map.prototype = {
