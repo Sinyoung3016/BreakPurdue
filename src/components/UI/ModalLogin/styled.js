df1196ad132d534c90e4dd3e4a380ddae2b998b6
parkeunsik <qkrdmstlr3@naver.com>
2021-11-07 15:10:44 +0900

    [feat] ModalLogin component 생성
    - 로그인을 담당하는 모달 컴포넌트 생성
    - Header에서 조건에 따라 렌더링

---
+++ src/components/UI/ModalLogin/styled.js
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 34rem;
+  height: 13rem;
+  border-radius: 10px;
+  background-color: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Title = styled.h2`
+  margin: 0;
+  margin-bottom: 1.5rem;
+  font-size: 2.5rem;
+  font-family: 'Comfortaa', cursive;
+`;
+
+export const Input = styled.input`
+  width: 27rem;
+  height: 3.7rem;
+  padding-left: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
