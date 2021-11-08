897036d2b075e9abe0fa500a350b5e2e57b7ac8c
parkeunsik <qkrdmstlr3@naver.com>
2021-11-09 00:59:07 +0900

    [feat] 검색기능 강화(핀 추가 등)
    - 입력 후 검색버튼을 누르면 검색된 리스트의 장소들이 하나씩 핀으로 지도에 찍힌다
    - 핀 하나를 누르면 모두 제거된다
    - 다른 검색 수행시 모두 제거된다

--- src/components/UI/Geocoder/styled.js
+++ src/components/UI/Geocoder/styled.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+export const Container = styled.form`
   position: absolute;
   top: 100px;
   right: 12px;
@@ -15,12 +15,26 @@ export const Container = styled.div`
 export const Input = styled.input`
   width: 100%;
   height: 100%;
-  padding-left: 2rem;
+  padding-left: 1rem;
+  padding-right: 4rem;
   margin-bottom: 12px;
 
   border: none;
   outline: none;
   border-radius: 10px;
+`;
+
+export const IconWrapper = styled.button`
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  right: 0.7rem;
+  top: 1rem;
+  width: 20px;
+  height: 20px;
+
+  border: none;
+  background-color: white;
 `;
 
 export const List = styled.ul`
