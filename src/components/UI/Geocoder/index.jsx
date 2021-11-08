a6f746c8495f271b619119dae5e66c6528887a77
parkeunsik <qkrdmstlr3@naver.com>
2021-11-08 19:12:20 +0900

    [feat] Geocoder 컴포넌트 생성
    - 위치 정보에 구글 지오코더를 사용하였다
    - autocomplete로 받아온 주소를 바탕으로 react-geocode를 이용해 좌표값을 가져온다

---
+++ src/components/UI/Geocoder/index.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import useGoogleAutocomplete from 'react-google-autocomplete/lib/usePlacesAutocompleteService';
+import addressToMarker from './geocoder';
+import * as Style from './styled';
+
+function Geocoder({ map }) {
+  const [value, setValue] = useState('');
+  const [searchedMarker, setSearchedMarker] = useState(undefined);
+  const { placePredictions, getPlacePredictions, isPlacePredictionsLoading } = useGoogleAutocomplete({
+    apiKey: process.env.GOOGLE_MAP_KEY,
+    language: 'en',
+    options: {
+      types: ['geocode', 'establishment'],
+      componentRestrictions: { country: 'us' },
+    },
+  });
+
+  const clickPlacePrediction = async (place) => {
+    setValue(place.description);
+    const marker = await addressToMarker(place.description, map);
+    if (searchedMarker) searchedMarker.remove();
+    setSearchedMarker(marker);
+  };
+
+  return (
+    <Style.Container>
+      <Style.Input
+        style={{ color: 'black' }}
+        value={value}
+        placeholder="검색 후 핀을 누르세요"
+        onChange={(evt) => {
+          getPlacePredictions({ input: evt.target.value });
+          setValue(evt.target.value);
+        }}
+      />
+      {!isPlacePredictionsLoading && !!placePredictions.length && (
+        <Style.List>
+          {placePredictions.map((place) => (
+            <Style.Item
+              key={place.description}
+              onClick={() => {
+                clickPlacePrediction(place);
+              }}
+            >
+              <h4>{place.description}</h4>
+            </Style.Item>
+          ))}
+        </Style.List>
+      )}
+    </Style.Container>
+  );
+}
+
+export default Geocoder;
---
+++ src/components/UI/Geocoder/styled.js
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  width: 240px;
+  height: 40px;
+
+  z-index: 3000;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  padding-left: 2rem;
+  margin-bottom: 12px;
+
+  border: none;
+  outline: none;
+  border-radius: 10px;
+`;
+
+export const List = styled.ul`
+  padding: 5px 10px;
+  margin: 0;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Item = styled.li`
+  cursor: pointer;
+`;
