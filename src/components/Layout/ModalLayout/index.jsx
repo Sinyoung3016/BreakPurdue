46830435a299b871e6907e34a6705ae2f331d1d3
parkeunsik <qkrdmstlr3@naver.com>
2021-11-08 23:05:29 +0900

    [refactor] Overlay 분리
    - Overlay를 ModalLayout에서 하나의 컴포넌트로 분리

--- src/components/Layout/ModalLayout/index.jsx
+++ src/components/Layout/ModalLayout/index.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Overlay from '../Overlay';
 import * as Style from './styled';
-
-function Overlay({ children, clickOverlay }) {
-  const modalTag = document.getElementById('modal');
-
-  return (
-    modalTag &&
-    ReactDOM.createPortal(
-      <>
-        <Style.Overlay aria-label="overlay" onClick={clickOverlay} />
-        {children}
-      </>,
-      modalTag,
-    )
-  );
-}
 
 function ModalLayout({ children, closeModal }) {
   return (
-    <Overlay clickOverlay={closeModal}>
+    <Overlay active clickOverlay={closeModal}>
       <Style.Container>{children}</Style.Container>
     </Overlay>
   );
