b7d2326de611bd1102a773305ca53abd8dc5006a
Fortune00 <tasha0415@naver.com>
2021-11-10 16:58:21 +0900

    [feat] dataProvider 생성
    - App.jsx에서 테스트 중

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import React, { useEffect } from 'react';
 import IndexPage from './pages/main';
 import GlobalStyle from './style/GlobalStyle';
 
+import { getRecords } from './dataProvider';
+
 // Routing필요 시 라이브러리 추가설치 필요
 function App() {
+  useEffect(() => {
+    getRecords();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
