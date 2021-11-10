11f84b642ceb8067feca60777fc11e71e50c96a3
Fortune00 <tasha0415@naver.com>
2021-11-10 16:57:24 +0900

    [refactor] 파일명 변경 및 버전관련 버그 처리

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseInit = {
   apiKey: process.env.FIREBASE_API_KEY,
