089eb955860067ed656cf5354d86e86d89a56c9e
parkeunsik <qkrdmstlr3@naver.com>
2021-11-15 16:59:54 +0900

    [fix] merge 후 에러 수정
    - 브랜치 머지한 후 발생하는 린트에러 등 수정

--- src/dataProvider/index.js
+++ src/dataProvider/index.js
@@ -12,44 +12,40 @@ const COMMENT = 'comment';
  * number numOfVisit
  * string place : 제목
  * number placeTag
- **/
+ * */
 
 export const getRecordList = async () => {
   const recordsSnapshot = await getDocs(collection(firestore, RECORDS));
   try {
     const recordList = recordsSnapshot.docs.map((d) => ({ ...d.data(), id: d.id }));
-    console.log('recordList', recordList);
     return recordList;
   } catch (e) {
-    console.log('Error from getRecordList', e);
+    return [];
   }
 };
 
 export const addNewRecord = async (cityTag, date, location, numOfVisit, place, placeTag) => {
   const newRecord = await addDoc(collection(firestore, RECORDS), {
-    cityTag: cityTag,
-    date: date,
-    location: location,
-    numOfVisit: numOfVisit,
-    place: place,
-    placeTag: placeTag,
+    cityTag,
+    date,
+    location,
+    numOfVisit,
+    place,
+    placeTag,
   });
-  console.log('newRecord', newRecord.id);
   return newRecord.id;
 };
 
 export const deleteRecord = async (recordID) => {
   await deleteDoc(doc(firestore, RECORDS, recordID));
-  console.log('deleteRecord', recordID);
 };
 
 export const getComment = async (recordID) => {
   try {
     const commentSnapshot = await getDocs(collection(firestore, RECORDS, recordID, COMMENT));
     const CommentList = commentSnapshot.docs.map((d) => d.data());
-    console.log('CommentList', CommentList);
     return CommentList;
   } catch (e) {
-    console.log('Error from getComment', e);
+    return [];
   }
 };
