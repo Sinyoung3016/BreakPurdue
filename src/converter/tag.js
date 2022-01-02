edeb5aeb8d684f0915bff613fc220a3a594276af
parkeunsik <qkrdmstlr3@naver.com>
2022-01-01 21:30:20 -0500

    [refactor] 핀 이미지 경로 변경
    - 핀 이미지에서 표시하는 경로 변경

--- src/converter/tag.js
+++ src/converter/tag.js
@@ -1,3 +1,11 @@
+import restaurantsImg from '../../public/images/restaurants.png';
+import barImg from '../../public/images/bars.png';
+import cafeImg from '../../public/images/cafe.png';
+import placesImg from '../../public/images/places.png';
+import photographyImg from '../../public/images/photography.png';
+import commercialImg from '../../public/images/commercial.png';
+import toursImg from '../../public/images/tours.png';
+
 export const CITY = ['라피엣', '시카고', '뉴욕', '인디애나폴리스'];
 export const PLACE = ['음식점', '술집', '카페', '명소', '놀이', '편의', '기타'];
 
@@ -20,19 +28,19 @@ export const placeTag2Num = (tag) => {
 export const placeTag2TagSrc = (tag) => {
   switch (tag) {
     case '음식점':
-      return 'url(/images/restaurants.png)';
+      return `url(${restaurantsImg})`;
     case '술집':
-      return 'url(/images/bars.png)';
+      return `url(${barImg})`;
     case '카페':
-      return 'url(/images/cafe.png)';
+      return `url(${cafeImg})`;
     case '명소':
-      return 'url(/images/places.png)';
+      return `url(${placesImg})`;
     case '놀이':
-      return 'url(/images/photography.png)';
+      return `url(${photographyImg})`;
     case '편의':
-      return 'url(/images/commercial.png)';
+      return `url(${commercialImg})`;
     case '기타':
-      return 'url(/images/tours.png)';
+      return `url(${toursImg})`;
     default:
       return '';
   }
@@ -42,19 +50,19 @@ export const placeNum2TagSrc = (num) => {
   const tag = placeNum2Tag(num);
   switch (tag) {
     case '음식점':
-      return 'url(/images/restaurants.png)';
+      return `url(${restaurantsImg})`;
     case '술집':
-      return 'url(/images/bars.png)';
+      return `url(${barImg})`;
     case '카페':
-      return 'url(/images/cafe.png)';
+      return `url(${cafeImg})`;
     case '명소':
-      return 'url(/images/places.png)';
+      return `url(${placesImg})`;
     case '놀이':
-      return 'url(/images/photography.png)';
+      return `url(${photographyImg})`;
     case '편의':
-      return 'url(/images/commercial.png)';
+      return `url(${commercialImg})`;
     case '기타':
-      return 'url(/images/tours.png)';
+      return `url(${toursImg})`;
     default:
       return '';
   }
